e3df35000de6eb9233754a480a52df60b50685f0
Ayush <ayushverma1016@gmail.com>
2024-10-14 22:23:56 +0530

    Describe the changes you made

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Navbar from '../Components/Navbar'
 import CategoryMenu from '../Components/CategoryMenu'
-import Fooditems from '../Components/Fooditems'
+import Fooditems from '../Components/FoodItems'
 import Cart from '../Components/Cart'
 
 const Home = () => {
